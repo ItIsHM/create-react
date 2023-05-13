8ff8266aaad3789a938eeaaba8e1df9ef35329ec
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-13 14:08:21 +0530

    Update SongPlayer.js

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const SongPlayer = () => {
   const { id } = useParams();
