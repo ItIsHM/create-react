36b6dc95e2a71166610d1c003e65cc2d8e7cf3dc
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-13 14:16:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import MusicSearch from './components/MusicSearch';
 import SongPlayer from './components/SongPlayer';
@@ -8,8 +8,10 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={MusicSearch} />
-        <Route path="/song/:id" component={SongPlayer} />
+        
+        <Route path='/' element={<MusicSearch/>} />
+        
+        <Route path='/song/:id' element={<SongPlayer/>} />
       </Switch>
     </Router>
   );
