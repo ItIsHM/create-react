9186cb44429d68828171f11d17de356625fa5823
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-13 12:54:33 +0530

    Update MusicSearch.js

--- src/components/MusicSearch.js
+++ src/components/MusicSearch.js
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+
+import axios from 'axios';
 const MusicSearch = () => {
   const [songName, setSongName] = useState('');
   const [results, setResults] = useState([]);
