ae72cb8dc7fd868a557e63435cadb206694d9b9a
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-13 14:22:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import SongPlayer from './components/SongPlayer';
 const App = () => {
   return (
     <Router>
-     <Layout>
+     
       <Routes>
         
         <Route path='/' element={<MusicSearch/>} />
         
         <Route path='/song/:id' element={<SongPlayer/>} />
             </Routes>
-          </Layout>
+          
     </Router>
   );
 };
