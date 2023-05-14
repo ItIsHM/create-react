f6d0689eca69a9460485d0c86562ce7ea8b24698
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-14 05:49:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import LoadingBar from 'react-top-loading-bar'
 import MusicSearch from './components/MusicSearch';
 import SongPlayer from './components/SongPlayer';
 
