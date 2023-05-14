856aa60b32a3027e71b7c3e71156041af4e24a3a
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-05-14 12:45:34 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
+import MusicSearch from './components/MusicSearch';
+import SongPlayer from './components/SongPlayer';
+
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <>
+      <LoadingBar
+        color='#1DB954'
+        progress={progress}
+        height={3}
+        shadow={false}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Router>
+        <Routes>
+          <Route path='/' element={<MusicSearch />} />
+          <Route path='/song/:id' element={<SongPlayer />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
