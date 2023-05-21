b9c92b6a7105492bc6978d2aaa7303489fbdbd9d
HM <95879255+HMser@users.noreply.github.com>
2023-05-21 11:42:19 +0530

    Update SongPlayer.js

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import download from 'js-file-download';
-import * as id3 from 'node-id3';
+import { parseBlob } from 'music-metadata-browser';
+import { ID3Writer } from 'browser-id3-writer';
 
 const SongPlayer = () => {
   const { id } = useParams();
@@ -32,7 +33,7 @@ const SongPlayer = () => {
   const handleDownload = async (url, buttonId) => {
     try {
       const response = await axios.get(url, {
-        responseType: 'arraybuffer',
+        responseType: 'blob',
         onDownloadProgress: (progressEvent) => {
           const progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
           const button = document.getElementById(buttonId);
@@ -40,31 +41,46 @@ const SongPlayer = () => {
           button.disabled = true;
         },
       });
-      const songBuffer = Buffer.from(response.data);
+      const songBlob = response.data;
       const filename = `${song.name} - ${song.primaryArtists.split(',')[0]}.mp3`;
-
-      const imageData = await axios.get(song.image[2].link, {
-        responseType: 'arraybuffer',
-      });
-      const imageBuffer = Buffer.from(imageData.data);
 
       const tags = {
         title: song.name,
         artist: song.primaryArtists,
-        image: {
-          mime: 'image/jpeg', // Adjust the mime type if necessary
-          type: {
-            id: 3, // The ID3 tag type for album art
-            name: 'front cover',
-          },
-          description: 'Album Cover',
-          imageBuffer,
-        },
         // Add more metadata fields as needed
       };
 
-      id3.write(tags, songBuffer);
-      download(songBuffer, filename);
+      const metadata = await parseBlob(songBlob);
+      metadata.common.title = tags.title;
+      metadata.common.artist = tags.artist;
+
+      if (song.image.length > 0) {
+        const imageData = await axios.get(song.image[2].link, {
+          responseType: 'blob',
+        });
+        const imageBlob = imageData.data;
+        const imageType = imageData.headers['content-type'];
+        metadata.common.picture = [
+          {
+            data: imageBlob,
+            format: imageType,
+            description: 'Album Cover',
+          },
+        ];
+      }
+
+      const updatedFile = new Blob([songBlob], { type: metadata.type });
+      const updatedTags = {
+        ...metadata.common,
+        ...tags,
+      };
+
+      const writer = new ID3Writer(updatedFile);
+      writer.setTags(updatedTags);
+      writer.addTag();
+
+      const taggedSongBlob = writer.getBlob();
+      download(taggedSongBlob, filename);
     } catch (error) {
       console.error('An error occurred while downloading the song:', error);
     }
@@ -73,7 +89,9 @@ const SongPlayer = () => {
   if (loading) {
     return (
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-        <div style={{ fontSize: '24px', textAlign: 'center', animation: 'blink-animation 1s infinite' }}>Loading...</div>
+        <div style={{ fontSize: '24px', textAlign: 'center', animation: 'blink-animation 1s infinite' }}>
+          Loading...
+        </div>
       </div>
     );
   }
@@ -85,6 +103,7 @@ const SongPlayer = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>{song.name}</h1>
+    
       <img src={song.image[1].link} alt="Song Thumbnail" style={{ width: '150px', height: '150px' }} />
       <p>Artist: {song.primaryArtists}</p>
       <div>
