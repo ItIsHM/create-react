d241957b64e7d1d92eb0853d0b0620654d1cdb67
HM <95879255+HMser@users.noreply.github.com>
2023-05-21 12:07:36 +0530

    Update SongPlayer.js

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -7,6 +7,7 @@ const SongPlayer = () => {
   const { id } = useParams();
   const [song, setSong] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedQuality, setSelectedQuality] = useState(null);
 
   useEffect(() => {
     const fetchSong = async () => {
@@ -15,12 +16,15 @@ const SongPlayer = () => {
         const data = response.data;
         if (data && data.status === 'SUCCESS' && data.data && data.data.length > 0) {
           setSong(data.data[0]);
+          setSelectedQuality(data.data[0].downloadUrl[0].quality);
         } else {
           setSong(null);
+          setSelectedQuality(null);
         }
       } catch (error) {
         console.error('An error occurred:', error);
         setSong(null);
+        setSelectedQuality(null);
       } finally {
         setLoading(false);
       }
@@ -47,6 +51,10 @@ const SongPlayer = () => {
     }
   };
 
+  const handleQualityChange = (quality) => {
+    setSelectedQuality(quality);
+  };
+
   if (loading) {
     return (
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
@@ -65,8 +73,55 @@ const SongPlayer = () => {
       <img src={song.image[1].link} alt="Song Thumbnail" style={{ width: '150px', height: '150px' }} />
       <p>Artist: {song.primaryArtists}</p>
       <div>
+        <select
+          value={selectedQuality}
+          onChange={(e) => handleQualityChange(e.target.value)}
+          style={{
+            padding: '8px',
+            borderRadius: '4px',
+            margin: '16px',
+            fontSize: '16px',
+          }}
+        >
+          {song.downloadUrl.map((item) => (
+            <option key={item.quality} value={item.quality}>
+              {item.quality}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '32px' }}>
+        <button
+          style={{
+            backgroundColor: '#ff5a5f',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4',
+                      padding: '16px 24px',
+            fontSize: '16px',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            cursor: 'pointer',
+          }}
+        >
+          Play
+        </button>
         {song.downloadUrl.map((item, index) => (
-          <button key={item.quality} onClick={() => handleDownload(item.link, `downloadButton-${index}`)} id={`downloadButton-${index}`}>
+          <button
+            key={item.quality}
+            onClick={() => handleDownload(item.link, `downloadButton-${index}`)}
+            id={`downloadButton-${index}`}
+            style={{
+              backgroundColor: '#4caf50',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '4px',
+              padding: '8px 16px',
+              margin: '8px',
+              fontSize: '14px',
+              cursor: 'pointer',
+            }}
+          >
             Download {item.quality}
           </button>
         ))}
@@ -76,3 +131,4 @@ const SongPlayer = () => {
 };
 
 export default SongPlayer;
+
